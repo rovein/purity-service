2e5c29cb816d16f949f040871a35a8bf5462cc3c
Roman Kuznetsov <roman.kuznetsov@nure.ua>
2021-03-13 00:01:26 +0200

    Report for lab2 established, minor changes

--- backend/src/main/java/ua/nure/CleanUpApplication.java
+++ backend/src/main/java/ua/nure/CleanUpApplication.java
@@ -8,32 +8,7 @@ import org.springframework.context.ApplicationContext;
 public class CleanUpApplication {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = SpringApplication.run(CleanUpApplication.class, args);
-
-//        ContractRepository contractRepository = applicationContext.getBean(ContractRepository.class);
-//
-//        Set<ContractInfoDto> allContractsByCustomerCompany = contractRepository.getAllContractsByCleaningCompany("cleaning_company@gmail.com");
-//
-//        for (ContractInfoDto contractInfoDto : allContractsByCustomerCompany) {
-//            System.out.println(contractInfoDto.getId());
-//        }
-//
-//        System.out.println(allContractsByCustomerCompany);
-
-
-//        AdminRepository adminRepository = applicationContext.getBean(AdminRepository.class);
-//        BCryptPasswordEncoder passwordEncoder = applicationContext.getBean(BCryptPasswordEncoder.class);
-//
-//        Admin admin = new Admin();
-//
-//        admin.setId(1L);
-//        admin.setEmail("admin@gmail.com");
-//        admin.setPassword(passwordEncoder.encode("adminadmin"));
-//        admin.setFirstName("Rick");
-//        admin.setLastName("Sanchez");
-//        admin.setRole(new Role(1L, UserRole.ADMIN));
-//
-//        adminRepository.save(admin);
+        SpringApplication.run(CleanUpApplication.class, args);
     }
 
 
