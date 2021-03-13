8a204eb72232f57fcaca2f1b67e011cce427d633
Roman Kuznetsov <roman.kuznetsov@nure.ua>
2021-03-13 23:06:55 +0200

    Changed DB URL and project name

--- backend/src/main/java/ua/nure/PurityServiceApplication.java
+++ backend/src/main/java/ua/nure/PurityServiceApplication.java
@@ -2,13 +2,12 @@ package ua.nure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class CleanUpApplication {
+public class PurityServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CleanUpApplication.class, args);
+        SpringApplication.run(PurityServiceApplication.class, args);
     }
 
 
