c4bfc8a10b4d3d5d706d16e3e0a3fa363e77962b
Roman Kuznetsov <roman.a.kuznetsov@nixsolutions.com>
2021-05-19 20:00:01 +0300

    Redesigned MoreInfo, added switching between indicators and contracts

--- frontend/src/View/Components/placement-owner/MoreInfo.js
+++ frontend/src/View/Components/placement-owner/MoreInfo.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { Line, Circle } from 'rc-progress';
 import {withTranslation} from 'react-i18next';
 import Loader from "react-loader-spinner";
+import Button from "../ui/Button";
+import Moment from "moment";
+import localization from 'moment/locale/uk';
 
 var url = "http://localhost:8080"
 
@@ -12,8 +15,11 @@ class Info extends React.Component {
     this.state = {
       error: null,
       isLoaded: false,
+      showIndicators: true,
+      showContracts: false,
       room: {},
-      device: {}
+      device: {},
+      contracts: []
     };
   }
 
@@ -41,7 +47,32 @@ class Info extends React.Component {
             error
           });
         }
-      )
+      );
+
+    const email = JSON.parse(localStorage.getItem("placementOwner")).email
+
+    fetch(`${url}/contracts/placement-owner/${email}`, {
+      method: 'get',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.getItem('Token')
+      }
+    })
+      .then(res => res.json())
+      .then(  (result) => {
+          this.setState({
+            contracts: result.filter(contract => {
+              return contract.placementId === Number.parseInt(localStorage.getItem("Id"))
+            }),
+          });
+          console.log(this.state.contracts)
+        },
+        (error) => {
+          this.setState({
+            error
+          });
+        })
   }
 
   render() {
@@ -59,26 +90,88 @@ class Info extends React.Component {
     }
     return (<div>
         <div className="profile_back">
-          <p id="cName">{t("Rooms")} № {this.state.room.id}, {this.state.room.placementType}</p>
+          <p id="cName">
+            {t("Rooms")} № {this.state.room.id}, {this.state.room.placementType}<br/>
+          </p>
           <p></p>
-          <p>{t("Temp")}: {this.state.device.temperature} ℃</p>
-          <p></p>
-          <p>{t("Hum")}: {this.state.device.humidity} %</p>
-          <p></p>
-          <p>{t("AdjFact")}: {this.state.device.adjustmentFactor}</p>
+          <p>
+            {t("Temp")}: {this.state.device.temperature} ℃<br/><br/>
+            {t("Hum")}: {this.state.device.humidity} %<br/><br/>
+            {t("AdjFact")}: {this.state.device.adjustmentFactor}
+          </p>
+          <p>
+            <Button
+              text = "Переглянути показники"
+              disabled = {false}
+              onClick = { () => {this.setState({showIndicators: true, showContracts: false})}}
+            />
+            <Button
+              text = "Переглянути угоди"
+              disabled = {false}
+              onClick = { () => {this.setState({showIndicators: false, showContracts: true})}}
+            />
+          </p>
         </div>
-        <div className={"progress-grid"}>
-          <div>
-            <h2 style={{paddingLeft: "60px"}}>{t("AirQ")}: {this.state.device.airQuality * 100} %</h2>
-            <Circle style={{width:400, height: 400}} percent={this.state.device.airQuality * 100} strokeWidth="4" strokeColor="#2d52f5" />
+        { this.state.showIndicators &&
+          <div className={"progress-grid"}>
+            <div>
+              <h2
+                style={{paddingLeft: "60px"}}>{t("AirQ")}: {this.state.device.airQuality * 100} %</h2>
+              <Circle style={{width: 400, height: 400}}
+                      percent={this.state.device.airQuality * 100}
+                      strokeWidth="4" strokeColor="#2d52f5"/>
+            </div>
+            <div>
+              <h2>{t("dFact")}: {this.state.device.dirtinessFactor * 100} %</h2>
+              <Circle style={{width: 400, height: 400}}
+                      percent={this.state.device.dirtinessFactor * 100}
+                      strokeWidth="4" strokeColor="#2d52f5"/>
+            </div>
           </div>
-         <div>
-           <h2>{t("dFact")}: {this.state.device.dirtinessFactor * 100} %</h2>
-          <Circle style={{width:400, height: 400}} percent={this.state.device.dirtinessFactor * 100} strokeWidth="4" strokeColor="#2d52f5" />
-         </div>
-        </div>
+        }
+        {this.state.showContracts && <table className="w3-table-all w3-centered w3-large">
+          <thead>
+          <tr>
+            <th>ID</th>
+            <th>{t("sId")}</th>
+            <th>{t("serviceName")}</th>
+            <th>{t("CComp")}</th>
+            <th>{t("rId")}</th>
+            <th>{t("Price")}</th>
+            <th>{t("CDate")}</th>
+          </tr>
+          </thead>
+          <tbody>
+          {this.state.contracts.sort((oneContract, anotherContract) => {
+            return oneContract.id - anotherContract.id
+          }).map((contract) => {
+            const columnStyle = {verticalAlign: "middle"};
+            this.localTime(contract.date)
+            return (
+              <tr>
+                <td style={columnStyle}>{contract.id}</td>
+                <td style={columnStyle}>{contract.providerServiceId}</td>
+                <td style={columnStyle}>{contract.serviceName}</td>
+                <td style={columnStyle}>{contract.cleaningProviderName}</td>
+                <td style={columnStyle}>{contract.placementId}</td>
+                <td style={columnStyle}>{contract.price}</td>
+                <td style={columnStyle}>{this.state.date}</td>
+              </tr>
+            );
+          })}
+          </tbody>
+        </table>
+        }
       </div>
     )
+  }
+
+  localTime(date) {
+    if (localStorage.getItem("i18nextLng") === 'EN') {
+      this.state.date = Moment(date).locale('en').format('LLL')
+    } else if (localStorage.getItem("i18nextLng") === 'UA') {
+      this.state.date = Moment(date).locale("uk", localization).format('LLL')
+    }
   }
 }
 
