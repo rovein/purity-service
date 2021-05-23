6d62d8f584ec4b4166fdee5b71d277da8607bb6e
Roman Kuznetsov <roman.kuznetsov@nure.ua>
2021-05-23 13:46:29 +0300

    Small name refactoring for Admin.

--- frontend/src/View/Components/admin/AdminProfile.js
+++ frontend/src/View/Components/admin/AdminProfile.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { withTranslation } from 'react-i18next';
-import CleaningCard from './AdminClCard';
-import AdminCustCart from './AdminCustCart';
+import CleaningCard from './AdminCleaningProvidersTable';
+import AdminCustCart from './AdminPlacementOwnersTable';
 import axios from "axios";
 import Button from "../ui/Button";
 
