dbfd861e1a56ae095342849650a6f86986cdd1fe
Roman Kuznetsov <roman_kuznetsov6@epam.com>
2022-01-31 23:31:45 +0200

    Added axios single instance with base URL and Auth header.

---
+++ frontend/src/View/Components/util/Api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import {SERVER_URL} from "./Constants";
+import jwt_decode from "jwt-decode";
+
+const instance = axios.create({
+    baseURL: SERVER_URL
+})
+instance.interceptors.request.use(config => {
+    console.log(config)
+    if (localStorage.getItem("Token") != null) {
+        const token = localStorage.getItem("Token");
+        console.log(token)
+        const decoded = jwt_decode(token);
+        config.headers.common.Authorization = `Bearer ${token}`;
+        config.headers['Accept'] = "application/json";
+        config.headers['Content-Type'] = "application/json";
+    }
+    return config;
+},error => Promise.reject(error));
+export default instance;
